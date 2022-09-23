0eb34ad4560ba6f375d43c563ebb4038fa9254d6
unknown <douglaswebdev1@gmail.com>
2022-09-23 13:25:36 -0300

    nubank commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const card = document.querySelector(".container");
+
+card.addEventListener("mousemove", cardEffect);
+function cardEffect(event){
+const cardWidth = card.offsetWidth;
+const cardHeight = card.offsetHeight;
+const centerX = card.offsetLeft + cardWidth / 2;
+const centerY = card.offsetTop + cardHeight / 2;
+const positionX = event.clientX - centerX;
+const positionY = event.clientY - centerY;
+
+
+const rotateX  = ((+1)*25*positionY/(cardHeight/2).toFixed(2));
+const rotateY = ((-1)*25*positionX/(cardWidth/2).toFixed(2));
+card.style.transform = `perspective(500px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+
+
+
+
+
+}
+
